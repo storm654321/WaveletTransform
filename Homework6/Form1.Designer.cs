3a1313b9a1d8f35a2cdcd927ea24596c4ec17ac8
storm654321 <storm654321@gmail.com>
2017-08-23 13:39:40 +0800

    加入專案檔案。

---
+++ Homework6/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Homework6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(1020, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "執行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(628, 590);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(646, 79);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(825, 591);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(914, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(88, 20);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(814, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "小波次數";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(145, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(387, 62);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "選擇其他圖片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(709, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "頻率域亮度增加倍率";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1.0",
+            "1.5",
+            "2.0",
+            "2.5",
+            "3.0",
+            "3.5",
+            "4.0",
+            "4.5",
+            "5.0"});
+            this.comboBox2.Location = new System.Drawing.Point(914, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(88, 20);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1369, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "小波逆轉";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1248, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "破壞圖片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1369, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "存檔";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1487, 688);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Homework6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Homework6/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+namespace Homework6
+{
+    public partial class Form1 : Form
+    {
+        Bitmap a, b, c;
+        Rectangle clone;
+        public Form1()
+        {
+            InitializeComponent();
+            if (System.IO.File.Exists(Application.StartupPath + @"\lena_gray.jpg"))
+            {
+                a = new Bitmap("lena_gray.jpg");
+                pictureBox1.Image = a;
+                clone = new Rectangle(0, 0, a.Width, a.Height);
+            }
+        }
+        int[,,] show;
+        double[,,] output;
+        double[,,] source;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //檢查吃進來的圖示不是奇數，是奇數把它Resize成偶數圖片
+            int x=0,y=0;
+            if (a.Width % 2 == 1)
+                x = 1;
+            if (a.Height % 2 == 1)
+                y = 1;
+            Size newSize = new Size(a.Width + x, a.Height + y);
+            a = (new Bitmap(a,newSize));
+            show= GetRGBData(a);
+            output = new double[a.Width, a.Height, 3];
+            source = new double[a.Width, a.Height, 3];
+            //把input轉成double的
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                        source[i, j, k] = show[i, j, k];
+            double[,,] output_B = new double[a.Width, a.Height, 3];
+
+            int tempA, tempB,tempC,tempD;
+            int roundMax = comboBox1.SelectedIndex;
+            int roundMaxB = comboBox2.SelectedIndex;
+            //程式碼核心過程  第一個For來跑做幾次小波
+            for (int round = 1; round <= roundMax+1; round++)
+            {
+                tempA = (int)(a.Width / Math.Pow(2, round));
+                tempB= (int)(a.Height / Math.Pow(2, round - 1));
+                tempC = (int)(a.Width / Math.Pow(2, round - 1));
+                tempD= (int)(a.Height / Math.Pow(2, round));
+                for (int i = 0; i <tempA ; i++)
+                    for (int j = 0; j < tempB; j++)
+                        for (int k = 0; k < 3; k++)
+                            output_B[i , j, k] = (source[i * 2, j, k] + source[i * 2 + 1, j, k]) / 2;
+                for (int i = 0; i < tempA; i++)
+                    for (int j = 0; j < tempB; j++)
+                        for (int k = 0; k < 3; k++)
+                            output_B[i + a.Width / (int)(2* Math.Pow(2, round - 1)), j, k] = (source[i * 2, j, k] - source[i * 2 + 1, j, k]) / 2;
+                for (int i = 0; i < tempC; i++)
+                    for (int j = 0; j < tempD; j++)
+                        for (int k = 0; k < 3; k++)
+                            source[i, j, k] = (output_B[i, j * 2, k] + output_B[i, j * 2 + 1, k]) / 2;
+                for (int i = 0; i < tempC; i++)
+                    for (int j = 0; j < tempD; j++)
+                        for (int k = 0; k < 3; k++)
+                            source[i, j + a.Height / (int)(2 * Math.Pow(2, round - 1)), k] = (output_B[i, j * 2, k] - output_B[i, j * 2 + 1, k]) / 2;
+            }
+            //做好之後的結果，輸出到output，用絕對值把負的弄掉
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        output[i, j, k] = (int)(Math.Abs(source[i, j, k]) * (0.5+0.5*roundMaxB));
+                        if (output[i, j, k] > 255)
+                            output[i, j, k] = 255;
+                    }
+            for (int i = 0; i < a.Width / Math.Pow(2,1+ roundMax); i++)
+                for (int j = 0; j < a.Height / Math.Pow(2,1+ roundMax); j++)
+                    for (int k = 0; k < 3; k++)
+                        output[i, j, k] = source[i, j, k];
+
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                        show[i, j, k] = (int)output[i, j, k];
+            b =setRGBData(show);
+            pictureBox3.Image = b;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[,,] source = GetRGBData(b);
+            int[,,] output = new int[a.Width, a.Height, 3];
+            int[,,] output_B = new int[a.Width, a.Height, 3];
+
+           
+            c = setRGBData(output_B);
+            pictureBox3.Image = c;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileDialog tomato = new OpenFileDialog();
+            if(tomato.ShowDialog()==DialogResult.OK)
+            {
+                a = new Bitmap(tomato.FileName);
+                pictureBox1.Image = a;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (pictureBox3.Image == null)
+            {
+                MessageBox.Show("請先執行轉換");
+                return;
+            }
+            int roundMax = comboBox1.SelectedIndex;
+            double[,,] out_temp = new double[a.Width, a.Height, 3];
+
+            int tempWidth= a.Width;
+            int tempHeight= a.Height;
+            int tempDivide;
+            int tempMax=roundMax;
+            int tempheight;
+            for (int round = 1; round <= roundMax +1; round++)
+            {
+
+
+                tempDivide = (int)Math.Pow(2, tempMax);
+                tempMax--;
+                tempheight = a.Height / tempDivide;
+                tempWidth = a.Width / tempDivide;
+                for (int i = 0; i < a.Width/tempDivide; i++)
+                    for (int j = 0; j < a.Height / tempDivide; j = j + 2)
+                    {
+                        for (int k = 0; k < 3; k++)
+                            out_temp[i, j, k] = source[i, j / 2, k] + source[i, j / 2 + tempheight / 2, k];
+                    }
+                for (int i = 0; i < a.Width/ tempDivide; i++)
+                    for (int j = 0; j < a.Height/ tempDivide; j = j + 2)
+                    {
+                        for (int k = 0; k < 3; k++)
+                            out_temp[i, j + 1, k] = source[i, j / 2, k] - source[i, j / 2 + tempheight/ 2, k];
+                    }
+                for (int i = 0; i < a.Width / tempDivide; i = i + 2)
+                    for (int j = 0; j < a.Height / tempDivide; j++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                            source[i, j, k] = out_temp[i / 2, j, k] + out_temp[i / 2 + tempWidth / 2, j, k];
+                    }
+                for (int i = 0; i < a.Width / tempDivide; i = i + 2)
+                    for (int j = 0; j < a.Height / tempDivide; j++)
+                    {
+                        for (int k = 0; k < 3; k++)
+                            source[i + 1, j, k] = out_temp[i / 2, j, k] - out_temp[i / 2 + tempWidth / 2, j, k];
+                    }
+            }
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (source[i, j, k] < 0)
+                            source[i, j, k] = 0;
+                        else if (source[i, j, k] >255)
+                            source[i, j, k] = 255;
+                    }
+            //c = setRGBData(source);
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                        show[i, j, k]=(int)source[i, j, k]  ;
+
+            c = setRGBData(show);
+            pictureBox3.Image = c;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3.Image == null)
+            {
+                MessageBox.Show("請先執行轉換");
+                return;
+            }
+            Random crandom = new Random();
+            int aaa;
+            Random kkk = new Random();
+            for (int i = a.Width/4; i < a.Width/2; i++)
+            {
+                for (int j = 0; j < a.Height; j++)
+                {
+                    for(int k=0;k<3;k++)
+                    {
+                        source[i, j, k] = 0;
+                    }
+                }
+            }
+            for (int i=0;i<a.Width;i++)
+            {
+                for(int j=0;j<a.Height;j++)
+                {
+                    if (kkk.Next(10) <= 6)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            aaa = crandom.Next(100);
+                            if ((source[i, j, k] + (-100 + aaa) < 255) && (source[i, j, k] + (-100 + aaa) > 0))
+                            {
+                                source[i, j, k] = source[i, j, k] + (-100 + aaa);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for(int k=0;k<3;k++)
+                        output[i, j, k] = Math.Abs(source[i, j, k]);
+
+            for (int i = 0; i < a.Width; i++)
+                for (int j = 0; j < a.Height; j++)
+                    for (int k = 0; k < 3; k++)
+                        show[i, j, k] = (int)output[i, j, k];
+            pictureBox3.Image = setRGBData(show);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox3.Image == null)
+            {
+                MessageBox.Show("請先做轉換");
+                return;
+            }
+
+            else
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "所有檔案|*.*|BMP File|*.bmp|JPEG File|*.jpg|GIF File|*.gif|PNG File|*.png|TIFF File|*.tiff";
+                saveFileDialog1.FilterIndex = 3;
+                if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK && saveFileDialog1.FileName != "")
+                {
+                    Bitmap processedBitmap = c;
+                    switch (saveFileDialog1.FilterIndex)
+                    {
+                        case 1:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case 2:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        case 3:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        case 4:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        case 5:
+                            processedBitmap.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Tiff);
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static int[,,] GetRGBData(Bitmap bitImg)
+        {
+            int height = bitImg.Height;
+            int width = bitImg.Width;
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            // get the starting memory place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //scan width
+            int stride = bitmapData.Stride;
+            //scan ectual
+            int widthByte = width * 3;
+            // the byte num of padding
+            int skipByte = stride - widthByte;
+            //set the place to save values
+            int[,,] rgbData = new int[width, height, 3];
+            #region
+            unsafe//專案－＞屬性－＞建置－＞容許Unsafe程式碼須選取。
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        //B channel
+                        rgbData[i, j, 2] = p[0];
+                        p++;
+                        //g channel
+                        rgbData[i, j, 1] = p[0];
+                        p++;
+                        //R channel
+                        rgbData[i, j, 0] = p[0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return rgbData;
+        }
+        public static Bitmap setRGBData(int[,,] rgbData)
+        {
+            Bitmap bitImg;
+            int width = rgbData.GetLength(0);
+            int height = rgbData.GetLength(1);
+            bitImg = new Bitmap(width, height, PixelFormat.Format24bppRgb);// 24bit per pixel 8x8x8
+            //locking
+            BitmapData bitmapData = bitImg.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            //get image starting place
+            IntPtr imgPtr = bitmapData.Scan0;
+            //image scan width
+            int stride = bitmapData.Stride;
+            int widthByte = width * 3;
+            int skipByte = stride - widthByte;
+            #region
+            unsafe
+            {
+                byte* p = (byte*)(void*)imgPtr;
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i < width; i++)
+                    {
+                        p[0] = (byte)rgbData[i, j, 2];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 1];
+                        p++;
+                        p[0] = (byte)rgbData[i, j, 0];
+                        p++;
+                    }
+                    p += skipByte;
+                }
+
+            }
+            bitImg.UnlockBits(bitmapData);
+            #endregion
+            return bitImg;
+        }
+    }
+}
+//FileDialog tomato = new OpenFileDialog();
+//if (tomato.ShowDialog() == DialogResult.OK)
+//{
+
+
+//    pictureBox1.Image = a;
+//    clone = new Rectangle(0, 0, a.Width, a.Height);
+//}
